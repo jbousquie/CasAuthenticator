af73a64cc38afe35e0958db7e8acc2955f6bb96e
Jerome Bousquie <jerome@bousquie.fr>
2023-09-08 11:18:46 +0200

    chgt délais

--- cas_login.py
+++ cas_login.py
@@ -152,18 +152,19 @@ class ServiceAuthenticator:
         service_session = self.service_session
         # modif pour le bug Ohris : 2 requêtes explicites sans redirect autorisés
         print(f'caslogin.py : ServiceAuthenticator.getAuthenticatedService() : envoi du ST CAS à{redirection_url}')
+        time.sleep(2)
         g_service = service_session.get(redirection_url, headers=service_headers, allow_redirects=False, proxies=proxy)
         status_code = 0
-        attempts = 4
+        max_attempts = 3
+        attempts = 0
         print(f'cas_login.py : ServiceAuthenticator.getAuthenticatedService() : tentative d\'accès à {service}')
-        time.sleep(2)
-        while status_code != 200 and attempts > 0:
-            attempts -= 1
-            time.sleep(2.5)
+        while status_code != 200 and attempts < max_attempts:
+            attempts += 1
+            time.sleep(3)
             g_service = service_session.get(service, headers=service_headers, allow_redirects=False, proxies=proxy)
             status_code = g_service.status_code
             self.authenticated_response = g_service
-            print('  > essai : ', str(4 - attempts), 'status code : ', str(status_code))
+            print('  > essai : ', str(attempts), 'status code : ', str(status_code))
             # print(g_service.cookies.get('PHPSESSID'))
             # print(g_service.cookies.get('SERVERID177380')) # Ce cookie est nécessaire pour le punch d'entrée, mais n'est parfois pas reçu du serveur
         self.status_code = status_code
